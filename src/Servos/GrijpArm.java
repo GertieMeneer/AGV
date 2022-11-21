b9baf39f0951eba8d7a3466adf5158f7a271630d
Rick <rrhvanhoof@gmail.com>
2022-11-21 13:22:09 +0100

    Grijparm begin

---
+++ src/Servos/GrijpArm.java
@@ -0,0 +1,11 @@
+package Servos;
+
+import TI.BoeBot;
+import TI.PinMode;
+
+public class GrijpArm {
+    public static void main(String[] args) {
+        BoeBot.setMode(3, PinMode.Output);
+
+    }
+}
