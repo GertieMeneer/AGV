70fd6947138a69aa0a515010a79f49e89c6b0aa8
victorxvtz <victorxvtz@gmail.com>
2022-11-21 16:05:38 +0100

    notities afstelling boebot

--- src/Servos/GrijpArm.java
+++ src/Servos/GrijpArm.java
@@ -5,9 +5,14 @@ import TI.PinMode;
 import TI.Servo;
 
 public class GrijpArm {
+
+
     public static void main(String[] args) {
-        Servo grijphaak = new Servo(13);
-        grijphaak.start();
+
+            Servo grijphaak = new Servo(14);
+            grijphaak.update(2300); // 12 rechterwiel boven 1500 achteruit. andere precies omgekeerd, grijparm 1700 dicht, 2300 open
+
 
     }
 }
+
