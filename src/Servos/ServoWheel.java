dac60d287a5cd028164cc33202b51a7a038a7367
Sander2482039 <sanderderegt@home.nl>
2022-11-21 12:48:19 +0100

    eerste deel

--- src/Servos/ServoWheel.java
+++ src/Servos/ServoWheel.java
@@ -5,8 +5,20 @@ import TI.*;
 public class ServoWheel {
     private Servo leftWheel = new Servo(12);
     private Servo rightWheel = new Servo(13);
+    private boolean isInside;
 
-    public ServoWheel(){
+    public ServoWheel(){}
+
+
+
+    public void driveForwards(){
+        if(this.isInside){
+            this.leftWheel.update(1700);
+            this.rightWheel.update(1300);
+        }else{
+            this.leftWheel.update(1550);
+            this.rightWheel.update(1450);
+        }
 
     }
 }
