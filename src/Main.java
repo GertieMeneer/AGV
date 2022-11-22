f7dfdd131482eb34dff4c055eb6d63f559f404db
GertieMeneer <gertjan.vat@icloud.com>
2022-11-22 10:14:03 +0100

    added comments to tentacles class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import additional.LED;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        LED led = new LED(1, true);
     }
 
     public void test() {
--- src/sensors/Tentacles.java
+++ src/sensors/Tentacles.java
@@ -6,13 +6,13 @@ import static TI.PinMode.Input;
 public class Tentacles {
     private int GPIOpin;
 
-    public Tentacles(int GPIOpin) {
+    public Tentacles(int GPIOpin) {  //here you can create a new tentacle object and assign it to a pin
         this.GPIOpin = GPIOpin;
         BoeBot.setMode(GPIOpin, Input);
     }
 
     public boolean checkTentacle() {
-        return BoeBot.digitalRead(GPIOpin);
-    }
+        return !BoeBot.digitalRead(GPIOpin);
+    } //this methode will return true or false depending on the state of the tentacle being pressed or not
 
 }
