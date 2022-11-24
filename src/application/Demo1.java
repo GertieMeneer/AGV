98628a0471bf5567abadc19692e4869273666078
Rick <rrhvanhoof@gmail.com>
2022-11-24 12:25:50 +0100

    if minder, skill issue

--- src/application/Demo1.java
+++ src/application/Demo1.java
@@ -14,42 +14,36 @@ public class Demo1 {
     }
 
     public static void distanceDrivingDemo() {
-        boolean driving = false;        //pretty much useless, morgen ff checken
+        boolean driving = false;
         Ultrasoon ultrasoon = new Ultrasoon(1, 0, 1000);
-//        LED errorLED = new LED(8, true);
 
         Servo rightWheel = new Servo(12);
         Servo leftWheel = new Servo(13);
         while (true) {
             if (!ultrasoon.checkDistance()) {
-                if (!driving) {
-
                     rightWheel.update(2000);
                     leftWheel.update(1000);
                     NeoPixels.allBlack();
                     NeoPixels.forwardWhite();
-                }
             } else {
                 rightWheel.update(1500);
                 leftWheel.update(1500);
-                driving = false;
                 NeoPixels.allRed();
-
             }
             BoeBot.wait(100);
         }
     }
 
-//    public static void grabingCraneDemo() {
-//        GrabingCrane.open();
-//        BoeBot.wait(1000);
-//        GrabingCrane.slowlyClose();
-//        BoeBot.wait(1000);
-//        GrabingCrane.slowlyOpen();
-//        BoeBot.wait(1000);
-//        GrabingCrane.close();
-//        BoeBot.wait(1000);
-//    }
+    public static void grabingCraneDemo() {
+        GrabingCrane.open();
+        BoeBot.wait(1000);
+        GrabingCrane.slowlyClose();
+        BoeBot.wait(1000);
+        GrabingCrane.slowlyOpen();
+        BoeBot.wait(1000);
+        GrabingCrane.close();
+        BoeBot.wait(1000);
+    }
 }
 
 
