828c9c5bf256b0dbfafea176f1976e1011e00622
Sander2482039 <sanderderegt@home.nl>
2022-11-25 12:08:25 +0100

    twee aansturende klassen

---
+++ src/interfacing/CollisionController.java
@@ -0,0 +1,7 @@
+package interfacing;
+
+import hardware.sensors.Ultrasone;
+
+public class CollisionController {
+    private Ultrasone ultsone;
+}
---
+++ src/interfacing/NotificationsController.java
@@ -0,0 +1,12 @@
+package interfacing;
+
+import hardware.additional.LED;
+import hardware.additional.NeoPixels;
+
+import java.util.ArrayList;
+
+public class NotificationsController {
+  private ArrayList<NeoPixels> pixels;
+
+
+}
