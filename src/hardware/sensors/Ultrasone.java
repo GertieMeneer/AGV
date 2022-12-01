79c34c5ba9c85298c39e94c31fbf970e26923fb4
Sander2482039 <sanderderegt@home.nl>
2022-12-01 11:01:42 +0100

    Aanpassing

--- src/hardware/sensors/Ultrasone.java
+++ src/hardware/sensors/Ultrasone.java
@@ -11,14 +11,14 @@ public class Ultrasone {
     private int echoPin;
     private int distance;
 
-    public Ultrasone(int triggerPin, int echoPin, int distance) {
+    public Ultrasone(int triggerPin, int echoPin) {
         this.triggerPin = triggerPin;
         this.echoPin = echoPin;
         this.distance = distance;
     }
 
 
-    public boolean checkDistance() {
+    public int checkDistance() {
         BoeBot.setMode(this.echoPin, Input);                        //setting pinmodes, doesnt work in constructor
         BoeBot.setMode(this.triggerPin, Output);
 
@@ -29,10 +29,11 @@ public class Ultrasone {
         int pulse = BoeBot.pulseIn(echoPin, true, 10000);   //reading pulse, future update: calculating distance in cm
         System.out.println(pulse);                                  //printing pulse for debugging/information
 
-        if (pulse < this.distance) {                                 //returning true or false depending on distance
-            return true;
-        }                                                           //lets method know if there is an object when it calls this method
-        return false;
+        return pulse;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
     }
 
     public int getDistance() {
