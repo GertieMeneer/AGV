71ead3c9244baa69979531ae1f310870fd8876be
victorxvtz <victorxvtz@gmail.com>
2022-12-08 10:18:38 +0100

    changes to IR

--- src/Demo1.java
+++ src/Demo1.java
@@ -2,8 +2,8 @@ import TI.BoeBot;
 import TI.PinMode;
 import TI.Servo;
 import TI.Timer;
-import hardware.additional.Bluetooth;
 import hardware.additional.Button;
+import hardware.sensors.IR;
 import hardware.sensors.Linesensor;
 import hardware.sensors.Ultrasone;
 import hardware.servos.GrabbingCrane;
@@ -11,6 +11,7 @@ import hardware.servos.Wheel;
 import interfacing.*;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Demo1 {
@@ -20,24 +21,24 @@ public class Demo1 {
     }
 
     public Demo1() {
-        PathTracker pt = new PathTracker();
-        CollisionController collisionController = new CollisionController();
-//        OverrideController button = new OverrideController(10);
-        BluetoothController bluetoothController = new BluetoothController("COM8");
-        Bluetooth bluetooth = new Bluetooth();
+//        PathTracker pt = new PathTracker();
+//        CollisionController collisionController = new CollisionController();
+        IR remote = new IR(2);
+//        Drive drive = new Drive();
+//
+//        drive.turnRight();
+//        BoeBot.wait(5000);
+        while (true) {
+              remote.readIRLes();
+              BoeBot.wait(10);
 
-        while(true) {
 //            pt.checkLine();
 //            BoeBot.wait(50);
 //            collisionController.checkDistance();
 //            BoeBot.wait(50);
-//            button.checkButton();
-//            BoeBot.wait(50);
-            bluetooth.readBluetoothModule();
-            BoeBot.wait(1);
-
         }
 
 
     }
+
 }
--- src/hardware/sensors/IR.java
+++ src/hardware/sensors/IR.java
@@ -6,38 +6,54 @@ import TI.PinMode;
 import java.util.ArrayList;
 
 public class IR {
+    private int number;
 
-    private int pin1;
-    private int pin2;
-
-    public IR(int pin1, int pin2) {
-        this.pin1 = pin1;
-        this.pin2 = pin2;
-        BoeBot.setMode(pin1, PinMode.Input);
-        BoeBot.setMode(pin2, PinMode.Output);
-    }
+    private int pin;
 
     public IR(int pin1) {
-        this.pin1 = pin1;
+        this.pin = pin;
         BoeBot.setMode(pin1, PinMode.Input);
+        BoeBot.setMode(6, PinMode.Output);
+
     }
 
     public int readIR() {
         //returns value received by IR
         int number = 0;
-        int pulselength = BoeBot.pulseIn(pin1, false, 6000);
-        if (pulselength > 2000){
+        int pulselength = BoeBot.pulseIn(pin, false, 6000);
+//        System.out.println(pulselength);
+        if (pulselength < 2000){
+            System.out.println();
             int lengths[] = new int[12];
             for (int i = 0; i < 12; i++) {
-                lengths[i] = BoeBot.pulseIn(0, false, 20000);
+                lengths[i] = BoeBot.pulseIn(pin, false, 20000);
             }
-
-            return translate(lengths);
+//            number = translate(lengths);
+            System.out.println(lengths);
+            return number;
+        }else{
+            return 0;
         }
-        return 0;
+
     }
 
-    private static int translate (int values[]){
+    public int[] readIRLes() {
+        IR ir = new IR(2);
+        System.out.println("Listening....");
+        while (true) {
+            int pulseLen = BoeBot.pulseIn(2, false, 6000);
+            if (pulseLen > 2000) {
+                int lengths[] = new int[12];
+                for (int i = 0; i < 12; i++) {
+                    lengths[i] = BoeBot.pulseIn(2, false, 20000);
+                }
+
+                number = ir.translate(lengths);
+            }
+        }
+    }
+
+    private int translate (int values[]){
         int number = 0;
         int bitCounter = 1;
         ArrayList<Integer> name = new ArrayList<>();
@@ -50,11 +66,18 @@ public class IR {
                 name.add(-1);
             }
         }
+        System.out.println(name);
+
+
 
         for (Integer value : name) {
             number += value * bitCounter;
             bitCounter = bitCounter + 8;
         }
+        if(number < 0) {
+            return 0;
+        }
+        System.out.println(number);
 
         return number;
     }
