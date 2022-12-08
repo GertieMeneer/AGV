69963e268965dad20a04da88316d511393bb0b84
GertieMeneer <gertjan.vat@icloud.com>
2022-12-08 18:56:03 +0100

    cleaned demo class

--- src/Demo1.java
+++ src/Demo1.java
@@ -17,31 +17,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Demo1 {
-    private PathTracker pt = new PathTracker();
-    private CollisionController collisionController = new CollisionController();
-    private IR remote = new IR(2);
-    private Drive drive = new Drive();
-    private Bluetooth bluetooth = new Bluetooth();
-    private Crane crane = new Crane(14);
 
     public static void main(String[] args) {
         new Demo1();
     }
 
     public Demo1() {
-        while (true) {
-            pt.checkLine();
-
-//            crane.open();
-//            BoeBot.wait(3000);
-            crane.close();
-//            BoeBot.wait(3000);
-
-//            collisionController.checkDistance();
-
-//            bluetooth.readBluetoothModule();
-//            BoeBot.wait(1);
-
-        }
+        //put test code here
+        //lol
+        //yes
     }
 }
