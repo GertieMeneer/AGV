705ac7a27c2b45406d0bd4875896ac804b9b225a
Sander2482039 <sanderderegt@home.nl>
2022-12-08 23:16:56 +0100

    BLUETOOTH WORKING OMGGGGGGGGGG

---
+++ src/application/Menu/MenuActions/DebugActions/UltraSoundDebug.java
@@ -0,0 +1,40 @@
+package application.Menu.MenuActions.DebugActions;
+
+import TI.BoeBot;
+import hardware.sensors.Ultrasone;
+import interfacing.NotificationsController;
+
+import java.awt.*;
+
+public class UltraSoundDebug {
+    private Ultrasone us;
+    private int pulseTranslation;
+
+    public UltraSoundDebug() {
+        this.us = new Ultrasone(11,10);
+    }
+
+
+    public void debug() {
+        NotificationsController nc = new NotificationsController();
+            this.pulseTranslation = us.checkDistance();
+            if (pulseTranslation < 1000){
+                nc.allRed();
+            }else if (pulseTranslation > 1000 && pulseTranslation < 2500){
+                nc.allBlue();
+            }else{
+                nc.allBlack();
+            }
+            BoeBot.wait(200);
+
+
+    }
+
+    public static void main(String[] args) {
+        UltraSoundDebug usd = new UltraSoundDebug();
+        while(true){
+            usd.debug();
+        }
+
+    }
+}
--- src/interfacing/CollisionController.java
+++ src/interfacing/CollisionController.java
@@ -10,35 +10,20 @@ import static TI.PinMode.Output;
 public class CollisionController {
     private Ultrasone ultrasone;
     private Drive drive = new Drive();
-    private Timer timer = new Timer(50);
+    private int pulseTranslation;
 
     public CollisionController() {
         this.ultrasone = new Ultrasone(11, 10);
     }
-    private Crane crane = new Crane(14);
 
     public void checkDistance() {
-        System.out.println(this.ultrasone.checkDistance());
-        if (this.ultrasone.checkDistance() < 500) {
+        this.pulseTranslation = ultrasone.checkDistance();
+        if(pulseTranslation < 500) {
             drive.emergencyBrake();
-        } else if (this.ultrasone.checkDistance() < 1500) {
+        } else if (pulseTranslation < 1500) {
             drive.slowStop();
         } else {
             drive.driveForwardSlowSpeed();
         }
     }
-
-    public void checkDistsanceCart() {
-        System.out.println(this.ultrasone);
-        if (this.ultrasone.checkDistance() < 500) {
-            drive.emergencyBrake();
-        } else if (this.ultrasone.checkDistance() < 1500) {
-            crane.open();
-            BoeBot.wait(1000);
-            crane.close();
-        } else {
-            drive.driveForwardSlowSpeed();
-        }
-    }
 }
-
